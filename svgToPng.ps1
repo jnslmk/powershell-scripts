5ce819311a4491e563065603158a3ea31569a03f
Jonas Lemke <jns.lemke@gmail.com>
2018-07-25 11:55:23 +0200

    Added svgToPng script

---
+++ svgToPng.ps1
@@ -0,0 +1,21 @@
+param (
+   [Parameter(Mandatory=$true)][string]$in
+)
+if (!(Test-Path $in)) {
+   Write-Host "Script interrupted - File" $in "does not exist"
+   exit
+}
+   
+$in = Convert-Path $in
+
+if (!([IO.Path]::GetExtension($in) -eq ".svg")) {
+   Write-Host "Script interrupted - Please provide a .svg-file"
+   exit
+}
+
+# Create output filename by changing extension
+$newExtension = "png"
+$out = [io.path]::ChangeExtension($in, $newExtension)
+
+Write-Host "Converting" $in "to" $newExtension ".."
+inkscape $in -z --export-area-drawing --export-png=$out -w 500
