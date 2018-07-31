f86da6e369f895040e665f7f8ae803f8febf19c3
Jonas Lemke <jns.lemke@gmail.com>
2018-07-31 09:56:06 +0200

    Added script to convert image to pdf files

---
+++ imgToPdf.ps1
@@ -0,0 +1,16 @@
+param (
+   [Parameter(Mandatory=$true)][string]$in
+)
+if (!(Test-Path $in)) {
+   Write-Host "Script interrupted - File" $in "does not exist"
+   exit
+}
+   
+$in = Convert-Path $in
+
+# Create output filename by changing extension
+$newExtension = "pdf"
+$out = [io.path]::ChangeExtension($in, $newExtension)
+
+Write-Host "Converting" $in "to" $newExtension ".."
+inkscape $in -z --export-area-drawing --export-pdf=$out
