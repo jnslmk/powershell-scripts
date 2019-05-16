e295895593c68a47e791efe9f6a49f27ab18f0d2
Jonas Lemke <jnslmk@users.noreply.github.com>
2019-05-16 09:53:11 +0200

    Using ghostscript directly for pdf → png

--- pdfToPng.ps1
+++ pdfToPng.ps1
@@ -13,6 +13,4 @@ $newExtension = "png"
 $out = [io.path]::ChangeExtension($in, $newExtension)
 
 Write-Host "Converting" $in "to" $newExtension ".."
-# Taken from https://stackoverflow.com/questions/6605006/convert-pdf-to-image-with-high-resolution
-# -sharpen 0x1.0 after $in may improve output with text pdfs
-magick -verbose -density 150 $in -trim -quality 100 -flatten $out
+gswin64 -sDEVICE=pngalpha -o $out -r300 $in
